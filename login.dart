6ba56190b0eea23887e28178d4b81cd348c498c0
sfazil24 <fazilshafiya2007@gmail.com>
2025-06-20 15:40:21 +0530

    Add files via upload

--- login.dart
+++ login.dart
@@ -1,35 +1,237 @@
 import 'package:flutter/material.dart';
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class User {
+  final String username;
+  final String password;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(
-        leading: Icon(Icons.add),
-        actions: [
+  User(this.username, this.password);
+}
 
-          Icon(Icons.camera),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(Icons.search),
-          ),
+class Database {
+  static List<User> users = [
+    User('user1', 'password1'),
+    User('user2', 'password2'),
+    User('user3', 'password3'),
+  ];
+  static void addUser(String username, String password) {
+    users.add(User(username, password));
+  }
 
-        ],
+  static bool isValidUser(String username, String password) {
+    return users.any((user) => user.username == username && user.password == password);
+  }
 
-      ),
-       body: Column(
-         children: [
-           TextFormField(),
-           TextFormField(),
-           SizedBox(height: 20,),
-           ElevatedButton(onPressed: (){}, child: Icon(Icons.add)),
-           SizedBox(height: 20,),
-           Image.asset("assets/email.jpeg")
-         ],
-       ),
-    );
-
+  static bool isExistingUser(String username) {
+    return users.any((user) => user.username == username);
   }
 }
 
+class LoginApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/signup': (context) => SignUpPage(),
+        '/welcome': (context) => WelcomePage(),
+      },
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  String _errorMessage = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your username';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    String username = _usernameController.text;
+                    String password = _passwordController.text;
+                    if (Database.isValidUser(username, password)) {
+                      Navigator.pushNamed(context, '/welcome', arguments: username);
+                    } else {
+                      setState(() {
+                        _errorMessage = 'Invalid username or password';
+                      });
+                    }
+                  }
+                },
+                child: Text('Login'),
+              ),
+              SizedBox(height: 10),
+              Text(
+                _errorMessage,
+                style: TextStyle(color: Colors.red),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account? "),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpPage extends StatefulWidget {
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  String _errorMessage = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign Up'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your username';
+                  } else if (Database.isExistingUser(value)) {
+                    return 'Username already exists';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    String username = _usernameController.text;
+                    String password = _passwordController.text;
+                    Database.addUser(username, password);
+                    Navigator.pop(context);
+                  }
+                },
+                child: Text('Sign Up'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final String username = ModalRoute.of(context)!.settings.arguments as String;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome, $username!',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.popUntil(context, ModalRoute.withName('/'));
+              },
+              child: Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- main.dart
+++ main.dart
@@ -1,75 +1,36 @@
-
-
-import 'package:fazil_project/1project.dart';
-import 'package:fazil_project/Amazon.dart';
-import 'package:fazil_project/Hi.dart';
-import 'package:fazil_project/Soft.dart';
-import 'package:fazil_project/Topic.dart';
-
-import 'package:fazil_project/Vali.dart';
-
-import 'package:fazil_project/am.dart';
-import 'package:fazil_project/as.dart';
-import 'package:fazil_project/bye.dart';
-import 'package:fazil_project/design.dart';
-import 'package:fazil_project/dup.dart';
-
-import 'package:fazil_project/gt.dart';
-import 'package:fazil_project/hello.dart';
-import 'package:fazil_project/Valid1.dart';
-import 'package:fazil_project/jkl-1.dart';
-import 'package:fazil_project/jkl-2gst.dart';
-import 'package:fazil_project/jkl-3BMI-1.dart';
-import 'package:fazil_project/jkl-6List-2.dart';
-import 'package:fazil_project/jkl-BMI-2simple.dart';
-import 'package:fazil_project/jkl.dart';
-
-import 'package:fazil_project/log.dart';
-import 'package:fazil_project/login.dart';
-import 'package:fazil_project/me.dart';
-import 'package:fazil_project/mm.dart';
-import 'package:fazil_project/name.dart';
-import 'package:fazil_project/page.dart';
-import 'package:fazil_project/ppp.dart';
-import 'package:fazil_project/pro.dart';
-import 'package:fazil_project/proj.dart';
-
-import 'package:fazil_project/screen/botnavi.dart';
-import 'package:fazil_project/software.dart';
-
-import 'package:fazil_project/to2.dart';
-import 'package:fazil_project/top1.dart';
-import 'package:fazil_project/vc.dart';
-import 'package:fazil_project/zzz.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
-import 'Task2.dart';
-import 'app.dart';
-
+import 'firebase_options.dart';
+import 'login.dart';
 
 
-import 'calcu.dart';
+Future<void> main() async{
 
-import 'inst.dart';
-import 'jkl-5List.dart';
-import 'loginScreen.dart';
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  runApp(  MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
 }
 
+class DefaultFirebaseOptions {
+  static var currentPlatform;
+}
+
+
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'App',
-        theme: ThemeData(
-          primarySwatch: Colors.red,
-        ),
-
-        home: Liss()
+      debugShowCheckedModeBanner: false,
+      home: LoginApp(),
     );
   }
 }
+
